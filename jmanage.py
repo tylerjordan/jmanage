b9353e6fe4859918461362f87a95eab1cf385717
Tyler Jordan <tjordan@juniper.net>
2016-09-20 09:03:15 -0400

    Initial commit

---
+++ jmanage.py
@@ -0,0 +1,271 @@
+__copyright__ = "Copyright 2016 Tyler Jordan"
+__version__ = "0.1.1"
+__email__ = "tjordan@juniper.net"
+
+import os
+import platform
+import subprocess
+import datetime
+import sys  # for writing to terminal
+import paramiko  # https://github.com/paramiko/paramiko for -c -mc -put -get
+import getpass  # for retrieving password input from the user without echoing back what they are typing.
+
+from ncclient import manager  # https://github.com/ncclient/ncclient
+from ncclient.transport import errors
+from netaddr import *
+from utility import *
+from prettytable import PrettyTable
+from jnpr.junos import *
+from jnpr.junos.exception import ConnectError
+from lxml import etree
+
+listDict = []
+listDictCSV = ".\\data\\listdict.csv"
+passCSV = ".\\data\\pass.csv"
+mypwd = ''
+myuser = ''
+port = 22
+
+
+def ping(ip):
+    """ Purpose: Determine if an IP is pingable
+
+    :param ip: IP address of host to ping
+    :return: True if ping successful
+    """
+    with open(os.devnull, 'w') as DEVNULL:
+        try:
+            # Check for Windows or Linux/MAC
+            ping_param = "-n" if platform.system().lower() == "windows" else "-c"
+            subprocess.check_call(
+                ['ping', ping_param, '3', ip],
+                stdout=DEVNULL,
+                stderr=DEVNULL
+            )
+            return True
+        except subprocess.CalledProcessError:
+            return False
+
+
+
+def information(connection, ip, software_info, host_name):
+    """ Purpose: This is the function called when using -info.
+                 It is grabs the model, running version, and serial number of the device.
+
+    :param: connection:    This is the ncclient manager connection to the remote device.
+            ip:            String containing the IP of the remote device, used for logging purposes.
+            software_info: A "show version" aka "get-software-information".
+            host_name:     The device host-name for output purposes.
+    :return: text of requested output
+    """
+    try:
+        model = software_info.xpath('//software-information/product-model')[0].text
+        junos_code = (software_info.xpath('//software-information/package-information/comment')[0].text.split('[')[1].split(']')[0])
+        chassis_inventory = connection.get_chassis_inventory(format='xml')
+        serial_number = chassis_inventory.xpath('//chassis-inventory/chassis/serial-number')[0].text
+        return {'host_name': host_name, 'ip': ip, 'model': model, 'junos_code': junos_code, 'serial_number': serial_number}
+    except:
+        print 'Host-name: {0} \nAccessed via: {1} \nDevice was reachable, the information was not found.'.format(host_name, ip)
+        return False
+
+
+def run(ip, username, password, port):
+    """ Purpose: To open an NCClient manager session to the device, and run the appropriate function against the device.
+        Parameters:
+            ip          -   String of the IP of the device, to open the connection, and for logging purposes.
+            username    -   The string username used to connect to the device.
+            password    -   The string password used to connect to the device.
+    """
+    try:
+        connection = manager.connect(host=ip,
+                                     port=port,
+                                     username=username,
+                                     password=password,
+                                     timeout=15,
+                                     device_params={'name': 'junos'},
+                                     hostkey_verify=False)
+        connection.timeout = 300
+    except errors.SSHError:
+        print 'Unable to connect to device: {0} on port: {1}'.format(ip, port)
+        return False
+    except errors.AuthenticationError:
+        print 'Bad username or password for device: {0}'.format(ip)
+        return False
+    except Exception as err:
+        print 'Unable to connect to device: {0} with error: {1}'.format(ip, err)
+        return False
+    else:
+        software_info = connection.get_software_information(format='xml')
+        host_name = software_info.xpath('//software-information/host-name')[0].text
+        output = information(connection, ip, software_info, host_name)
+        #print "Print Configuration"
+        #print connection.get_config(source='running', format='set')
+        return output
+
+
+def get_record(ip='', hostname='', sn=''):
+    """ Purpose: Returns a record from the listDict containing hostname, ip, model, version, serial number. Providing
+                three different methods to return the data.
+        Parameters:
+            ip          -   String of the IP of the device
+            hostname    -   String of the device hostname
+            sn          -   String of the device chassis serial number
+        Returns:
+            A dictionary containing the device data or 'False' if no record is found
+    """
+    print "Getting record for ip: {0}".format(ip)
+    if ip:
+        for record in listDict:
+            if record['ip'] == ip:
+                return record
+    elif hostname:
+        for record in listDict:
+            if record['host_name'] == hostname:
+                return record
+    elif sn:
+        for record in listDict:
+            if record['serial_number'] == sn:
+                return record
+    return False
+
+
+def scan_network(iplist):
+    pass
+
+
+def show_devices():
+    """ Purpose: Display a table showing devices with general facst.
+        Returns: Nothing
+    """
+    t = PrettyTable(['IP', 'Hostname', 'Model', 'Current Code', 'Serial Number', 'Last Updated'])
+    for device in listDict:
+        t.add_row([device['ip'], device['host_name'], device['model'], device['junos_code'], device['serial_number'], device['last_update']])
+    print t
+
+
+def add_record(ip):
+    """ Purpose: Adds a record to list of dictionaries
+        Returns: True or False
+    """
+    try:
+        items = run(ip, myuser, mypwd, port)
+        now = datetime.datetime.now()
+        items['last_update'] = now.strftime("%Y-%m-%d-%H%M")
+    except Exception as err:
+        print 'ERROR: Unable to get record information for: {0}'.format(ip)
+        return False
+    else:
+        listDict.append(items)
+        return True
+
+
+def check_ip(ip):
+    """ Purpose: Scans the device with the IP and handles action.
+        Returns: True or False
+    """
+    has_record = False
+    print "Recalling any stored records..."
+    if get_record(ip):
+        has_record = True
+
+    # If we can ping the IP...
+    if ping(ip):
+        # Try to collect current chassis info
+        print "Getting current information..."
+        remoteDict = run(ip, myuser, mypwd, port)
+
+        # If info could be collected...
+        if remoteDict:
+            # Check if this IP is associated with a record.
+            if has_record:
+                # If yes, check that the existing record is up-to-date
+                localDict = get_record(ip)
+                if localDict['host_name'] != remoteDict['host_name']:
+                    pass
+                if localDict['serial_number'] != remoteDict['serial_number']:
+                    pass
+                if localDict['model'] != remoteDict['model']:
+                    pass
+                if localDict['junos_code'] != remoteDict['junos_code']:
+                    pass
+
+            else:
+                # If no, this is a device that hasn't been identified yet, create a new record
+                print "Adding new device: {0}".format(ip)
+                if add_record(ip):
+                    print "Device Add Successful."
+                    return True
+                else:
+                    print "Device Add Failed."
+                    return False
+        else:
+            print "ERROR: Unable to collect information from device: {0}".format(ip)
+            return False
+    # If we can't ping, but we have a record
+    elif has_record:
+        # Set record status to "unreachable"
+        print "ERROR: Unable to ping KNOWN device: {0}".format(ip)
+        return False
+    # If we can't ping, and have no record
+    else:
+        print "ERROR: Unable to ping: {0}".format(ip)
+        return False
+
+
+def fetch_config(ip):
+    """ Purpose: Get current configuration from device.
+        Returns: Nothing
+    """
+    dev = Device(host=ip, user=myuser, passwd=mypwd)
+    # Try to open a connection to the device
+    try:
+        print "------------------------- Opening connection to: {0} -------------------------\n".format(ip)
+        print "User: {0}".format(myuser)
+        print "Pass: {0}".format(mypwd)
+        dev.open()
+    # If there is an error when opening the connection, display error and exit upgrade process
+    except ConnectError as err:
+        print "Cannot connect to device {0} : {1}".format(ip, err)
+    # If try arguments succeed...
+    else:
+        # Increase the default RPC timeout to accommodate install operations
+        dev.timeout = 600
+        # Get config and try to display
+        # Following command requires 15.1+
+        # myconfig = dev.rpc.get_configuration(dict(format='set'))
+        myconfig = dev.rpc.get_config()
+        print etree.tostring(myconfig)
+
+# START OF SCRIPT #
+if __name__ == '__main__':
+    passDict = user_pass(passCSV)
+    mypwd = passDict['pass']
+    myuser = passDict['user']
+    my_options = ['Display Database', 'Scan IPs', 'Save Database', 'Load Database', 'Fetch Config']
+    while True:
+        print "*" * 25 + "\n"
+        answer = getOptionAnswerIndex('Choose your poison', my_options)
+        print "\n" + "*" * 25
+        if answer == "1":
+            show_devices()
+        elif answer == "2":
+            my_network = getInputAnswer('Enter IP/Mask')
+            for myip in IPNetwork(my_network).iter_hosts():
+                output = check_ip(str(myip))
+                # output = run('10.134.227.230', user, pwd, port)
+                print listDict
+                print "Script Ends..."
+        elif answer == "3":
+            print "Saving Database to CSV..."
+            listdict_to_csv(listDict, listDictCSV)
+            print "Completed Save"
+        elif answer == "4":
+            print "Loading Database from CSV..."
+            listDict = csv_to_listdict(listDictCSV)
+            print "Completed Load"
+        elif answer == "5":
+            ip = getInputAnswer('Enter IP')
+            print "Fetching Configuration..."
+            fetch_config(ip)
+        else:
+            quit()
---
+++ utility.py
@@ -0,0 +1,289 @@
+# File: utility.py
+# Author: Tyler Jordan
+# Modified: 7/26/2016
+# Purpose: Assist CBP engineers with Juniper configuration tasks
+
+import sys, re, os, csv
+import fileinput
+import glob
+import code
+
+from os import listdir
+from os.path import isfile, join
+
+#--------------------------------------
+# ANSWER METHODS
+#--------------------------------------
+
+
+# Method for asking a question that has a single answer, returns answer
+def getOptionAnswer(question, options):
+    answer = ""
+    loop = 0
+    while not answer:
+        print question + '?:\n'
+        for option in options:
+            loop += 1
+            print '[' + str(loop) + '] -> ' + option
+        answer = raw_input('Your Selection: ')
+        if int(answer) >= 1 and int(answer) <= loop:
+            index = int(answer) - 1
+            return options[index]
+        else:
+            print "Bad Selection"
+            loop = 0
+
+
+# Method for asking a question that has a single answer, returns answer index
+def getOptionAnswerIndex(question, options):
+    answer = ""
+    loop = 0
+    while not answer:
+        print question + '?:\n'
+        for option in options:
+            loop += 1
+            print '[' + str(loop) + '] -> ' + option
+        answer = raw_input('Your Selection: ')
+        if int(answer) >= 1 and int(answer) <= loop:
+            return answer
+        else:
+            print "Bad Selection"
+            loop = 0
+
+
+# Method for asking a user input question
+def getInputAnswer(question):
+    answer = ""
+    while not answer:
+        answer = raw_input(question + '?: ')
+    return answer
+
+
+# Method for asking a Y/N question
+def getYNAnswer(question):
+    answer = ""
+    while not answer:
+        answer = raw_input(question + '?(y/n): ')
+        if answer == 'Y' or answer == 'y':
+            answer = 'y'
+        elif answer == 'N' or answer == 'n':
+            answer = 'n'
+        else:
+            print "Bad Selection"
+            answer = ""
+    return answer
+
+
+# Return list of files from a directory
+def getFileList(mypath):
+    fileList = []
+    try:
+        for afile in listdir(mypath):
+            if isfile(join(mypath,afile)):
+                fileList.append(afile)
+    except:
+        print "Error accessing directory: " + mypath
+
+    return fileList
+
+
+# Method for requesting IP address target
+def getTarget():
+    print 64*"="
+    print "= Scan Menu                                                    ="
+    print 64*"="
+    # Loop through the IPs from the file "ipsitelist.txt"
+    loop = 0
+    list = {};
+    for line in fileinput.input('ipsitelist.txt'):
+        # Print out all the IPs/SITEs
+        loop += 1
+        ip,site = line.split(",")
+        list[str(loop)] = ip;
+        print '[' + str(loop) + '] ' + ip + ' -> ' + site.strip('\n')
+
+    print "[c] Custom IP"
+    print "[x] Exit"
+    print "\n"
+
+    response = ""
+    while not response:
+        response = raw_input("Please select an option: ")
+        if response >= "1" and response <= str(loop):
+            return list[response]
+        elif response == "c":
+            capturedIp = ""
+            while not capturedIp:
+                capturedIp = raw_input("Please enter an IP: ")
+                return capturedIp
+        elif response == "x":
+            response = "exit"
+            return response
+        else:
+            print "Bad Selection"
+
+
+# Common method for accessing multiple routers
+def chooseDevices():
+    # Define the routers to deploy the config to (file/range/custom)
+    print "**** Configuration Deployment ****"
+    method_resp = getOptionAnswer('How would you like to define the devices', ['file', 'range', 'custom'])
+    ip_list = []
+    # Choose a file from a list of options
+    if method_resp == "file":
+        print "Defining a file..."
+        path = '.\ips\*.ips'
+        files=glob.glob(path)
+        file_resp = getOptionAnswer('Choose a file to use', files)
+
+        # Print out all the IPs/SITEs
+        for line in fileinput.input(file_resp):
+            ip_list.append(line)
+
+    # Define a certain range of IPs
+    elif method_resp == "range":
+        print "Defining a range..."
+
+    # Define one or more IPs individually
+    elif method_resp == "custom":
+        print 'Define using /32 IP Addresses'
+        answer = ""
+        while( answer != 'x' ):
+            answer = getInputAnswer('Enter an ip address (x) to exit')
+            if( answer != 'x'):
+                ip_list.append(answer)
+
+    # Print the IPs that will be used
+    loop = 1;
+    for my_ip in ip_list:
+        print 'IP' + str(loop) + '-> ' + my_ip
+        loop=loop + 1
+
+    return ip_list
+
+
+# Converts listDict to CSV file
+def listDictCSV(myListDict, filePathName, keys):
+    addKeys = True
+    if (os.path.isfile(filePathName)):
+        addKeys = False
+    try:
+        f = open(filePathName, 'a')
+    except:
+        print "ERROR: Failure opening file in append mode.\n"
+        print("Be sure {0} isn't open in another program.".format(filePathName))
+    else:
+        if addKeys:
+            #Write all the headings in the CSV
+            for akey in keys[:-1]:							# Runs for every element, except the last
+                f.write(akey + ",")							# Writes most elements
+            f.write(keys[-1])								# Writes last element
+            f.write("\n")
+
+        for part in myListDict:
+            for bkey in keys[:-1]:
+                #print "Key: " + bkey + "  Value: " + str(part[bkey])
+                f.write(str(part[bkey]) + ",")
+            f.write(str(part[keys[-1]]))
+            f.write("\n")
+        f.close()
+        print "\nCompleted appending to CSV."
+
+
+# Convert listDict to CSV file
+def listdict_to_csv(listDict, filePathName):
+    try:
+        fieldNames = listDict[0].keys()
+        with open(filePathName, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldNames)
+            writer.writeheader()
+            writer.writerows(listDict)
+        return True
+    except Exception as err:
+        print "ERROR: Problem writing to file {0} : {1}".format(filePathName, err)
+        return False
+
+
+# Converts CSV file to listDict
+def csv_to_listdict(filePathName):
+    emptyList = []
+    try:
+        with open(filePathName) as csvfile:
+            listDict = [{csvfile: v for csvfile, v in row.items()}
+                        for row in csv.DictReader(csvfile, skipinitialspace=True)]
+        return listDict
+    except Exception as err:
+        print "ERROR: Problem reading from file {0} : {1}".format(filePathName, err)
+        return emptyList
+
+
+# Gets a target code
+def getCode(device, mypath):
+    tar_code = ""
+
+    # Does not have a target code, let's ask for one
+    print("\n" + "*"*10)
+    print("Hostname: " + device.hostname)
+    print("IP: " + device.ip)
+    print("Model: " + device.model)
+    print("Current Code: " + device.curr_code)
+
+    fileList = getFileList(mypath)
+    if fileList:
+        tar_code = getOptionAnswer("Choose an image", fileList)
+    else:
+        print("No images available.")
+    print("*"*10 + "\n")
+
+    return tar_code
+
+
+# Analyze listDict and create statistics (Upgrade)
+def tabulateUpgradeResults(listDict):
+    statusDict = {'success_rebooted': [],'success_not_rebooted': [], 'connect_fails': [], 'software_install_fails': [], 'total_devices': 0}
+
+    for mydict in listDict:
+        if mydict['Connected'] == 'Y' and mydict['OS_installed'] == 'Y':
+            if mydict['Rebooted'] == 'Y':
+                statusDict['success_rebooted'].append(mydict['IP'])
+            else:
+                statusDict['success_not_rebooted'].append(mydict['IP'])
+        elif mydict['Connected'] == 'Y' and mydict['OS_installed'] == 'N':
+            statusDict['software_install_fails'].append(mydict['IP'])
+        elif mydict['Connected'] == 'N':
+            statusDict['connect_fails'].append(mydict['IP'])
+        else:
+            print("Error: Uncaptured Result")
+        # Every device increments this total
+        statusDict['total_devices'] += 1
+
+    return statusDict
+
+
+# Analyze listDict and create statistics (Reboot)
+def tabulateRebootResults(listDict):
+    statusDict = {'rebooted': [], 'not_rebooted': [], 'connect_fails': [], 'total_devices': 0}
+
+    for mydict in listDict:
+        if mydict['Connected'] == 'Y':
+            if mydict['Rebooted'] == 'Y':
+                statusDict['rebooted'].append(mydict['IP'])
+            else:
+                statusDict['not_rebooted'].append(mydict['IP'])
+        elif mydict['Connected'] == 'N':
+            statusDict['connect_fails'].append(mydict['IP'])
+        else:
+            print("Error: Uncaptured Result")
+        # Every device increments this total
+        statusDict['total_devices'] += 1
+
+    return statusDict
+
+# Get password dictionary
+def user_pass(pathFileName):
+    reader = csv.reader(open(pathFileName, 'r'))
+    passDict = {}
+    for row in reader:
+        k, v = row
+        passDict[k] = v
+    return passDict
